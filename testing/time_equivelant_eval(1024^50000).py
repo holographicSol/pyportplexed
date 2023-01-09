a0e7614c6d6450862b72b6cb0b19f6393472edd5
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-09 16:38:23 +0000

    Add files via upload

---
+++ testing/time_equivelant_eval(1024^50000).py
@@ -0,0 +1,22 @@
+import time
+
+t0 = time.perf_counter()
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+x = eval('1024**500000')
+x = str(x).encode('utf8')
+print('time taken:', time.perf_counter() - t0)
+
+# (8 operations: 1024**100000) 16.447089799999958 seconds
