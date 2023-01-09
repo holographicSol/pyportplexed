5fe95675700e46ef6c052e258806d77c7b636e35
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-09 16:37:43 +0000

    Add files via upload

--- devopment_1/program_x.py
+++ devopment_1/program_x.py
@@ -17,7 +17,7 @@ print('\nStarting Program X: Using PyPortPlexed to compute...')
 threads = th.start(start_port, n_threads, results_port)
 
 """ connect to n_threads processes and give them a workload """
-connections = th.connect(threads, '1024**100000')
+connections = th.connect(threads, '1024**500000')
 
 """ wait for the results to come in from n_threads ports """
 t0 = time.perf_counter()
@@ -27,9 +27,18 @@ print('Items in results:', len(results))
 print()
 
 """
+(8 operations: 1024**100000)
 my pyportplexed (python -OO program_x.py):
-(8 operations: 1024**100000) 2.0330955000026734 seconds
+    (8 operations: 1024**100000) 2.0330955000026734 seconds
 
 single thread (python -OO time_equivalent_eval(1024^10000).py):
-(8 operations: 1024**100000) 16.447089799999958 seconds
+    (8 operations: 1024**100000) 16.447089799999958 seconds
+
+
+(8 operations: 1024**500000)
+my pyportplexed (python -OO program_x.py):
+    (8 operations: 1024**500000) 58.40562380000483 seconds (<1 minute)
+
+single thread (python -OO time_equivalent_eval(1024^10000).py):
+    (8 operations: 1024**500000) 361.5826317000028 seconds (6+ minutes)
 """
