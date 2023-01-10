5d71f6bdb868fc92b6c74755b24e4760c846b226
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-10 11:00:35 +0000

    Add files via upload
    -- added variable buffer size for PyPortPlexed daemonic processes.
    -- added variable buffer size for PyPortPlexed results function.
    -- added a layer of security in results function to accept only from s.getsockname().
    -- refined start() to loop once to run daemonic processes.

---
+++ devopment_2/module_pyportplexed_thread.py
@@ -0,0 +1,32 @@
+""" Written by Benjamin Jack Cullen
+Intention: This program runs as n subprocess(s) to do work for a program.
+           This subprocess should return values back to a program like a thread would but over ports.
+Summary: Multiplex via ports.
+"""
+import socket
+import sys
+
+port = int(sys.argv[1])
+port_1 = int(sys.argv[2])
+buffer_size = int(sys.argv[3])
+
+s = socket.socket()
+host = socket.gethostname()
+s.bind((host, port))
+s.listen(3)
+
+""" Accept incoming connection """
+c, addr = s.accept()
+
+""" Decode the incoming message """
+con_rcv = bytes(c.recv(buffer_size)).decode('utf8')
+
+""" Do some work, in this example eval() is used statically (test purposes only) CAUTION. """
+ev = eval(con_rcv)
+
+""" Send the result back to the main program (note: final operation is as client) """
+s = socket.socket()
+host = socket.gethostname()
+s.connect((host, port_1))
+s.send(bytes(str(ev), encoding='utf-8'))
+s.close()
---
+++ devopment_2/program_x.py
@@ -0,0 +1,44 @@
+""" Written by Benjamin Jack Cullen
+Intention: This module (to be) distributes workloads to n spawned subprocesses and waits for values to be
+           returned.
+Summary: Multiplex via ports.
+Example program using PyPortPlexed modules.
+"""
+import time
+import module_pyportplexed as th
+
+start_port = 55555
+results_port = 12345
+n_threads = 8
+
+print('\nStarting Program X: Using PyPortPlexed to compute...')
+
+""" Start n_threads processes with args """
+threads = th.start(start_port, n_threads, results_port, buffer_size=1024)
+
+""" Connect to n_threads processes and give them a workload """
+t0 = time.perf_counter()
+connections = th.connect(threads, '1024**100000')
+
+""" Wait for the results to come in from n_threads ports """
+results = th.results(results_port, n_threads, buffer_size=1024)
+print('Time taken:', time.perf_counter() - t0)
+print('Items in results:', len(results))
+print()
+
+"""
+(8 operations: 1024**100000)
+my pyportplexed (python -OO program_x.py):
+    (8 operations: 1024**100000) 2.0330955000026734 seconds
+
+single thread (python -OO time_equivalent_eval(1024^10000).py):
+    (8 operations: 1024**100000) 16.447089799999958 seconds
+
+
+(8 operations: 1024**500000)
+my pyportplexed (python -OO program_x.py):
+    (8 operations: 1024**500000) 58.40562380000483 seconds (<1 minute)
+
+single thread (python -OO time_equivalent_eval(1024^50000).py):
+    (8 operations: 1024**500000) 361.5826317000028 seconds (6+ minutes)
+"""
